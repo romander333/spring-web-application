71990bc1f4ba089cfd23d16297e4093b186b4759
Luchko Roman <romander09@gmail.com>
2025-06-03 10:59:56 +0300

    pagination

--- src/main/java/com/kozak/mybookshop/controller/BookController.java
+++ src/main/java/com/kozak/mybookshop/controller/BookController.java
@@ -4,9 +4,13 @@ import com.kozak.mybookshop.dto.BookDto;
 import com.kozak.mybookshop.dto.BookSearchParametersDto;
 import com.kozak.mybookshop.dto.CreateBookRequestDto;
 import com.kozak.mybookshop.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Book management", description = "Endpoints for managing books")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/books")
@@ -26,33 +31,39 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping
-    public List<BookDto> getAll() {
-        return bookService.findAll();
+    @Operation(summary = "Get some books", description = "get a page of books")
+    public Page<BookDto> getAll(Pageable pageable) {
+        return bookService.findAll(pageable);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get book", description = "Get book by id")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.getById(id);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete book", description = "Delete book by id")
     public void deleteBookById(@PathVariable Long id) {
         bookService.deleteById(id);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update book", description = "Update book by id")
     public BookDto updateBookById(@PathVariable Long id,
                                   @RequestBody @Valid CreateBookRequestDto request) {
         return bookService.update(request, id);
     }
 
     @PostMapping
+    @Operation(summary = "Create a new book", description = "Create a new book")
     public BookDto createBook(@RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
     }
 
     @GetMapping("/search")
+    @Operation(summary = "Search book", description = "Search book by parameters")
     public List<BookDto> search(BookSearchParametersDto parameters) {
         return bookService.search(parameters);
     }
--- src/main/java/com/kozak/mybookshop/service/BookService.java
+++ src/main/java/com/kozak/mybookshop/service/BookService.java
@@ -3,12 +3,15 @@ package com.kozak.mybookshop.service;
 import com.kozak.mybookshop.dto.BookDto;
 import com.kozak.mybookshop.dto.BookSearchParametersDto;
 import com.kozak.mybookshop.dto.CreateBookRequestDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.util.List;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto requestDto);
 
-    List<BookDto> findAll();
+    Page<BookDto> findAll(Pageable pageable);
 
     BookDto getById(Long id);
 
