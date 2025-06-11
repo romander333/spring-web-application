95e8982e0a21381f51eb1c70abd9057c584390c3
Luchko Roman <romander09@gmail.com>
2025-06-11 15:31:48 +0300

    add swagger doc

--- src/main/java/com/kozak/mybookshop/controller/AuthController.java
+++ src/main/java/com/kozak/mybookshop/controller/AuthController.java
@@ -3,6 +3,8 @@ package com.kozak.mybookshop.controller;
 import com.kozak.mybookshop.dto.user.UserRegistrationRequestDto;
 import com.kozak.mybookshop.dto.user.UserResponseDto;
 import com.kozak.mybookshop.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "User management", description = "Endpoints for managing users")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
@@ -17,6 +20,7 @@ public class AuthController {
     private final UserService userService;
 
     @PostMapping("/registration")
+    @Operation(summary = "Register", description = "Register User")
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto requestDto) {
         return userService.registration(requestDto);
     }
